a8f9190626ab6ea74ed1e9a916fdde854ed07834
Elvin Ciqueira <elvin.ciqueira@ferreri.co>
2022-05-26 14:43:57 -0300

    feat: add design tokens

---
+++ src/theme/tokens/brands/index.js
@@ -0,0 +1,11 @@
+import * as theme1 from './theme1';
+import * as theme2 from './theme2';
+
+export const brands = {
+  theme1: {
+    ...theme1,
+  },
+  theme2: {
+    ...theme2,
+  },
+};
---
+++ src/theme/tokens/globals.js
@@ -0,0 +1,96 @@
+import * as colors from './brands';
+
+const space = {
+  spacingSizeQuarck: '4px',
+  spacingSizeNano: '8px',
+  spacingSizeXxxs: '16px',
+  spacingSizeXxs: '24px',
+  spacingSizeXs: '32px',
+  spacingSizeSm: '40px',
+  spacingSizeMd: '48px',
+  spacingSizeLg: '56px',
+  spacingSizeXl: '64px',
+  spacingSizeXxl: '80px',
+  spacingSizeXxxl: '120px',
+  spacingSizeHuge: '160px',
+  spacingSizeGiant: '200px',
+  spacingInsetSizeQuarckV: '4px',
+  spacingInsetSizeQuarckH: '4px',
+  spacingInsetSizeQuarckStack: '4px 4px',
+  spacingInsetSizeNanoV: '8px',
+  spacingInsetSizeNanoH: '8px',
+  spacingInsetSizeNanoStack: '8px 8px',
+  spacingInsetSizeXsV: '16px',
+  spacingInsetSizeXsH: '16px',
+  spacingInsetSizeXsStack: '16px 16px',
+  spacingInsetSizeSmV: '24px',
+  spacingInsetSizeSmH: '24px',
+  spacingInsetSizeSmStack: '24px 24px',
+};
+
+const shadows = {
+  shadowLevel1X: '0px',
+  shadowLevel1Y: '4px',
+  shadowLevel1B: '8px',
+  shadowLevel1Stack: '0px 4px 8px',
+  shadowLevel2X: '0px',
+  shadowLevel2Y: '8px',
+  shadowLevel2B: '24px',
+  shadowLevel2Stack: '0px 8px 24px',
+  shadowLevel3X: '0px',
+  shadowLevel3Y: '16px',
+  shadowLevel3B: '32px',
+  shadowLevel3Stack: '0px 16px 32px',
+  shadowLevel4X: '0px',
+  shadowLevel4Y: '16px',
+  shadowLevel4B: '48px',
+  shadowLevel4Stack: '0px 16px 48px',
+};
+
+const opacity = {
+  opacityLevelSemiopaque: '0.72',
+  opacityLevelIntense: '0.64',
+  opacityLevelMedium: '0.32',
+  opacityLevelLight: '0.16',
+  opacityLevelSemitransparent: '0.08',
+};
+
+const radii = {
+  borderRadiusNone: '0px',
+  borderRadiusSm: '8px',
+  borderRadiusMd: '16px',
+  borderRadiusLg: '24px',
+  borderRadiusPill: '500px',
+  borderRadiusCircular: '50%',
+  borderWidthNone: '0px',
+  borderWidthHairline: '1px',
+  borderWidthThin: '2px',
+  borderWidthThick: '4px',
+  borderWidthHeavy: '8px',
+};
+
+const lineHeights = {
+  lineHeightDefault: '100%',
+  lineHeightXs: '115%',
+  lineHeightSm: '120%',
+  lineHeightMd: '133%',
+  lineHeightLg: '150%',
+  lineHeightXl: '170%',
+  lineHeightXxl: '200%',
+};
+
+const fontSizes = {
+  fontSizeXxxs: '12px',
+  fontSizeXxs: '14px',
+  fontSizeXs: '16px',
+  fontSizeSm: '20px',
+  fontSizeMd: '24px',
+  fontSizeLg: '32px',
+  fontSizeXl: '40px',
+  fontSizeXxl: '48px',
+  fontSizeXxxl: '64px',
+  fontSizeDisplay: '80px',
+  fontSizeGiant: '96px',
+};
+
+export { space, shadows, opacity, radii, lineHeights, fontSizes, colors };
